291e3656f4089cc3b621f96a6052c9a3269ab4d6
Muhammad Farhan Sjaugi <farhansj@academicgrid.my>
2017-09-07 09:16:22 +0800

    Update provision.sh

--- provision.sh
+++ provision.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-sudo apt-get update && sudo apt-get install ubuntu-desktop -y && cd /opt && sudo wget http://download.virtualbox.org/virtualbox/5.1.14/VBoxGuestAdditions_5.1.14.iso && sudo mount VBoxGuestAdditions_5.1.14.iso -o loop /mnt && cd /mnt && sudo echo "yes" | sudo sh VBoxLinuxAdditions.run --nox11
+sudo apt-get update && sudo apt-get install ubuntu-desktop -y && cd /opt && sudo wget http://download.virtualbox.org/virtualbox/5.1.26/VBoxGuestAdditions_5.1.26.iso && sudo mount VBoxGuestAdditions_5.1.26.iso -o loop /mnt && cd /mnt && sudo echo "yes" | sudo sh VBoxLinuxAdditions.run --nox11
